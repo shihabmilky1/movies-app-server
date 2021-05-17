1b2fa6aac5a3fc43cea437c8a9e9c93fe5499c63
Shihab Milky <shihabmilky1@gmail.com>
2021-05-18 00:00:46 +0600

    updated

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://shihabmilky:shihab@cluster0.4czm1.mongodb.net/movie?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
+
+
+client.connect(err => {
+    const movie = client.db("movie").collection("movie");
+    app.get('/movie/:category', (req, res) => {
+        movie.find({ cetagory: req.params.category })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
